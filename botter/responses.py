5b2feb248a2104e8c95b74783b0f1108ea88ebc8
Valacor <valyrious.informatics@gmail.com>
2023-10-01 00:20:21 +0200

    Remove unnecessary files

--- botter/responses.py
+++
@@ -1,8 +0,0 @@
-def handle_request(message) -> str:
-    p_message = message.lower()
-
-    if (p_message == "Hello"):
-        return "Hi, how is it going"
-
-    if (p_message == "Help"):
-        return "How can I help you"    
