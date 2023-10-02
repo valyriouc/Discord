f1534e6a1035247c1c32f8492cb66dbb89b0b32b
Valacor <valyrious.informatics@gmail.com>
2023-10-02 15:00:18 +0200

    Fix include system

---
+++ homie/__init__.py
@@ -0,0 +1,4 @@
+import sys
+sys.path.append("C:\\Hackerspace\\Source\\Discord\\")
+
+import main
--- homie/homie.py
+++ homie/homie.py
@@ -1,31 +1,50 @@
+from typing import Any
+from discord.flags import Intents 
 import discord
 import socket
-import common.credentials as creds
-import common.bots as bots
 
+import sys
+sys.path.append("C:\\Hackerspace\\Source\\Discord\\")
+
+import common.bots as bots
+import common.credentials as creds
+
+class HomieBot(discord.Client):
+    def __init__(self) -> None:
+        intents = discord.Intents()
+        intents.dm_messages = True
+        intents.message_content = True
+        intents.guild_messages = True
+        intents.message_content = True
+        intents.members = True
+
+        super().__init__(intents=intents)
+
+    async def on_ready(self):
+        print(f"Homie is now running")
+    
+
+# TODO: Use class approach
 def run_discord_bot():
     info = creds.Credentials.get(
         "../auths.txt",
         bots.Bot.homie)
     
-    TOKEN = info["Token"]
+    # TOKEN = info["Token"]
 
-    intents = discord.Intents()
-    intents.dm_messages = True
-    intents.message_content = True
-    intents.guild_messages = True
-    intents.message_content = True
-    intents.members = True
+    # intents = discord.Intents()
+    # intents.dm_messages = True
+    # intents.message_content = True
+    # intents.guild_messages = True
+    # intents.message_content = True
+    # intents.members = True
 
-    client = discord.Client(intents=intents)
-
-    @client.event
-    async def on_ready():
-        print(f"{client.user} is now running")
+    # client = discord.Client(intents=intents)
     
-    @client.event
-    async def on_message(message):
-        if message.auther == client.user:
-            return
-        
-    client.run(TOKEN)
+    # @client.event
+    # async def on_message(message):
+    #     if message.auther == client.user:
+    #         return
+    
+    bot = HomieBot()
+    bot.run(info["Token"])
