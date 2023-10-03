5cce41200df0f17bb09cd0fce550446153d0945d
Valacor <valyrious.informatics@gmail.com>
2023-10-03 14:20:29 +0200

    Implement dataprovider abstraction

---
+++ homie/dataProvider.py
@@ -0,0 +1,14 @@
+
+class DataProvider:
+    def __init__(self):
+        pass
+
+    def request_data(self, data: dict):
+        # The actual data provider builds up the custom protocol request from the metadata
+        # sends this request to the application server 
+        # parses the response 
+        # and sends the data back to the bot with displays it in the chat 
+        print("Base class call")
+
+        
+
--- homie/homie.py
+++ homie/homie.py
@@ -9,8 +9,11 @@ sys.path.append("C:\\Hackerspace\\Source\\Discord\\")
 import common.bots as bots
 import common.credentials as creds
 
+import testing.testDataProvider as tdata
+import dataProvider as dp 
+
 class HomieBot(discord.Client):
-    def __init__(self) -> None:
+    def __init__(self, dataProvider: dp.DataProvider) -> None:
         intents = discord.Intents()
         intents.dm_messages = True
         intents.message_content = True
@@ -20,17 +23,39 @@ class HomieBot(discord.Client):
 
         super().__init__(intents=intents)
 
+        self.dataProvider = dataProvider
+
     async def on_ready(self):
         print(f"Homie is now running")
     
-    async def on_message(self, message):
+    async def on_message(self, message: discord.Message):
         if message.author == self.user:
             return
-        
-def run_discord_bot():
-    info = creds.Credentials.get(
+        if not message.content.startswith("!"):
+            return 
+        print(message.content)
+        data = self.__parse_message(message.content)  
+        output = self.dataProvider.request_data(data)
+        await message.reply(output)
+    
+    def __parse_message(self, message: str):
+        data = {}
+        parts = message.split(",")
+        for it in range(0, len(parts)):
+            keyword = parts[it].replace("!", "")
+            data[keyword] = ""
+        return data
+    
+info = creds.Credentials.get(
         "../auths.txt",
         bots.Bot.homie)
+    
+def run_testing_bot():
+    print("Running into testing")
+    bot = HomieBot(tdata.TestDataProvider())
+    bot.run(info["Token"])
 
+def run_discord_bot():
+    # smarthome data provider  
     bot = HomieBot()
     bot.run(info["Token"])
